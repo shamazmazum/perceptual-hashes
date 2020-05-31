98ce9b4807bc6a40eaac07859b8a62048d66676e
Vasily Postnicov <shamaz.mazum@gmail.com>
2020-05-31 22:37:24 +0300

    AHASH and DHASH can accept imago images as an argument

--- src/hashes.lisp
+++ src/hashes.lisp
@@ -8,50 +8,65 @@
 (defmethod convert-to-grayscale ((image grayscale-image))
   image)
 
-(defun ahash (pathname)
-  "Return aHash (average hash) of an image with the path @c(pathname).
+(declaim (ftype
+          (function ((or string pathname image))
+                    (values bit-vector &optional))
+          ahash dhash))
+
+(defun get-image (image)
+  (declare (type (or string pathname image) image))
+  (typecase image
+    (image image)
+    ((or string pathname)
+     (load-image image))))
+
+(defun thumbnail-pixels (image &optional (thumb-size +thumb-size+))
+  (declare (type image image)
+           (type unsigned-byte thumb-size))
+  (image-pixels
+   (resize
+    (convert-to-grayscale image)
+    thumb-size thumb-size)))
+
+(defun ahash (image)
+  "Return aHash (average hash) of an @c(image) which can be a string,
+a pathname or an @c(imago:image) object. If @c(image) is a string or a
+pathname, the image is loaded using this pathname.
 This algorithm is based on whenever a pixel is brighter or darker than
 the average luminance of all pixels."
-  (declare (type (or string pathname) pathname))
-  (let* ((thumbnail (resize
-                     (convert-to-grayscale
-                      (load-image pathname))
-                     +thumb-size+
-                     +thumb-size+))
-         (pixels (flatten (image-pixels thumbnail)))
+  (let* ((pixels
+          (flatten (thumbnail-pixels (get-image image))))
          (mean (floor (reduce #'+ pixels)
                       (length pixels))))
     (map 'bit-vector
          (lambda (x) (if (< x mean) 0 1))
          pixels)))
 
-(defun dhash (pathname)
-  "Return dHash of an image with the path @c(pathname).
+(defun dhash (image)
+  "Return aHash (average hash) of an @c(image) which can be a string,
+a pathname or an @c(imago:image) object. If @c(image) is a string or a
+pathname, the image is loaded using this pathname.
 This algorithm is based on whenever a pixel is brighter or darker than
 the neighbour pixels."
-  (declare (type (or string pathname) pathname))
-  (loop
-     with pixels = (image-pixels
-                    (resize
-                     (convert-to-grayscale
-                      (load-image pathname))
-                     (+ +thumb-size+ 2)
-                     (+ +thumb-size+ 2)))
-     with hash = (make-array (* +thumb-size+ +thumb-size+)
-                             :element-type 'bit)
-     with idx = 0
-     for i from 1 to +thumb-size+
-     do
-       (loop for j from 1 to +thumb-size+ do
-            (setf (aref hash idx)
-                  (if (< (* (aref pixels i j) 4)
-                         (+ (aref pixels (1- i) j)
-                            (aref pixels i (1- j))
-                            (aref pixels i (1+ j))
-                            (aref pixels (1+ i) j)))
-                      0 1))
-            (incf idx))
-     finally (return hash)))
+  (let ((hash (make-array (* +thumb-size+ +thumb-size+)
+                          :element-type 'bit)))
+    (loop
+       with pixels = (thumbnail-pixels
+                      (get-image image)
+                      (+ +thumb-size+ 2))
+       with idx = 0
+       for i from 1 to +thumb-size+
+       do
+         (loop for j from 1 to +thumb-size+ do
+              (setf (aref hash idx)
+                    (if (< (* (aref pixels i j) 4)
+                           (+ (aref pixels (1- i) j)
+                              (aref pixels i (1- j))
+                              (aref pixels i (1+ j))
+                              (aref pixels (1+ i) j)))
+                        0 1))
+              (incf idx)))
+    hash))
 
 (defun hamming-distance (hash1 hash2)
   "Calculate Hamming distance between two hashes."
